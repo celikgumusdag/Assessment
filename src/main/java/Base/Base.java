f28e342aa074a6b6edda256f5a164e458b355b89
Çelik Gümüşdağ <celikgumusdag@gmail.com>
2017-08-04 23:35:13 +0300

    Base methods

--- src/main/java/Base/Base.java
+++ src/main/java/Base/Base.java
@@ -1,6 +1,10 @@
 package Base;
 
+import Driver.DriverManager;
+import Driver.DriverManagerFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 
 public abstract class Base {
     protected static WebDriver driver;
@@ -11,5 +15,26 @@ public abstract class Base {
 
     public Base() {
         //empty constructor
+    }
+
+    @BeforeSuite
+    public void OpenDriver() {
+        try {
+            DriverManager driverManager;
+            driverManager = DriverManagerFactory.getManager();
+            driver = driverManager.getDriver();
+            driver.manage().window().maximize();
+        } catch (Throwable ex) {
+            System.out.println("Error: " + ex);
+        }
+    }
+
+    @AfterSuite
+    public void CloseDriver() {
+        driver.quit();
+    }
+
+    protected void getPage(String url){
+        driver.get(url);
     }
 }
