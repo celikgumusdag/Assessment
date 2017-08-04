e21a0d899220be40c8afe7107a8c7465c2208176
Çelik Gümüşdağ <celikgumusdag@gmail.com>
2017-08-04 22:58:34 +0300

    returning second actor from API

--- src/main/java/Test/ReadApi.java
+++ src/main/java/Test/ReadApi.java
@@ -3,28 +3,26 @@ package Test;
 import Base.Base;
 import Helpers.PropertyReader;
 import org.openqa.selenium.WebDriver;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.annotations.Test;
 
-public class ReadApi extends Base {
-    public ReadApi(WebDriver driver) {
-        super(driver);
+import java.util.Arrays;
+import java.util.List;
+
+public class ReadApi{
+
+    public static String getSecondActor() throws Exception {
+        List<String> actorList = Arrays.asList(getAllActors().split(","));
+        return actorList.get(1);
     }
 
-    public ReadApi() {
-        super();
-        //empty constructor
-    }
-
-    @Test
-    public static void getActors() throws Exception{
+    public static String getAllActors() throws Exception{
         String apiLink=PropertyReader.getProperties("api");
         String apiResponse = Api.BaseApi.CallAPI(apiLink);
 
         JSONObject jsonObject = new JSONObject(apiResponse);
 
-        //String actors=jsonObject.getJSONObject("Actors").toString();
-        System.out.println(apiResponse);
+        String actors=jsonObject.getString("Actors");
+        return actors;
     }
 }
