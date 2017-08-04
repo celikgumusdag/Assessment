215c569483781d5a7cc6ebe3a0aac434cce341cd
Çelik Gümüşdağ <celikgumusdag@gmail.com>
2017-08-04 23:34:40 +0300

    Reading from API

--- src/main/java/Test/ReadApi.java
+++ src/main/java/Test/ReadApi.java
@@ -1,28 +1,24 @@
 package Test;
 
-import Base.Base;
 import Helpers.PropertyReader;
-import org.openqa.selenium.WebDriver;
 import org.json.JSONObject;
-import org.testng.annotations.Test;
-
 import java.util.Arrays;
 import java.util.List;
 
-public class ReadApi{
+public class ReadApi {
 
     public static String getSecondActor() throws Exception {
         List<String> actorList = Arrays.asList(getAllActors().split(","));
         return actorList.get(1);
     }
 
-    public static String getAllActors() throws Exception{
-        String apiLink=PropertyReader.getProperties("api");
+    public static String getAllActors() throws Exception {
+        String apiLink = PropertyReader.getProperties("api");
         String apiResponse = Api.BaseApi.CallAPI(apiLink);
 
         JSONObject jsonObject = new JSONObject(apiResponse);
 
-        String actors=jsonObject.getString("Actors");
+        String actors = jsonObject.getString("Actors");
         return actors;
     }
 }
