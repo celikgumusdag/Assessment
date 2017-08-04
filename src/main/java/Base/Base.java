f60942a513556a9dd3410272319ece158f3b39fd
Çelik Gümüşdağ <celikgumusdag@gmail.com>
2017-08-04 20:51:12 +0300

    Base class

--- src/main/java/Base/Base.java
+++ src/main/java/Base/Base.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 
 public abstract class Base {
     protected static WebDriver driver;
-    protected static String LanguageDependence;
 
     public Base(WebDriver driver) {
         this.driver = driver;
