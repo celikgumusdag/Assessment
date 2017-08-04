0bfb5bcad2b3089439aa73d811a16fdd962883a2
Çelik Gümüşdağ <celikgumusdag@gmail.com>
2017-08-04 20:52:02 +0300

    Hooks

---
+++ src/main/java/Test/Run.java
@@ -0,0 +1,39 @@
+package Test;
+
+import Base.Base;
+import Driver.DriverManager;
+import Driver.DriverManagerFactory;
+import Driver.DriverType;
+import Helpers.PropertyReader;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class Run extends Base  {
+
+    public Run(WebDriver driver) {
+        super(driver);
+    }
+
+    public Run() {
+        super();
+        //empty constructor
+    }
+
+    @BeforeSuite
+    public void OpenDriver() {
+        try {
+            DriverManager driverManager;
+            driverManager = DriverManagerFactory.getManager(DriverType.FIREFOX);
+            driver = driverManager.getDriver();
+            driver.manage().window().maximize();
+        } catch (Throwable ex) {
+            System.out.println("Error: " + ex);
+        }
+    }
+
+    @AfterSuite
+    public void CloseDriver() {
+        driver.quit();
+    }
+}
