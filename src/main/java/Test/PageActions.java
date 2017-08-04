eccfdaf59072edcc0d58dc57dcff38ec974044ea
Çelik Gümüşdağ <celikgumusdag@gmail.com>
2017-08-05 01:53:32 +0300

    Page actions added

--- src/main/java/Test/PageActions.java
+++ src/main/java/Test/PageActions.java
@@ -2,12 +2,30 @@ package Test;
 
 import Base.Base;
 import Helpers.PropertyReader;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PageActions extends Base {
 
     @Test
     public void OpenPage() {
-        getPage(PropertyReader.getProperties("url"));
+        String url=PropertyReader.getProperties("url");
+        getPage(url);
+        Assert.assertEquals(getUrl(),url);
+    }
+
+    @Test (dependsOnMethods = { "OpenPage" })
+    public void SearchActor() throws Exception {
+        String url=PropertyReader.getProperties("actorlisturl");
+        fillTextBox("#navbar-query",ReadApi.getSecondActor());
+        clickButton("#navbar-submit-button");
+        Assert.assertEquals(getUrl(),url);
+    }
+
+    @Test (dependsOnMethods = { "SearchActor" })
+    public void ClickActor(){
+        String url=PropertyReader.getProperties("actorurl");
+        clickButton("tbody>tr:first-child>.result_text>a");
+        Assert.assertEquals(getUrl(),url);
     }
 }
